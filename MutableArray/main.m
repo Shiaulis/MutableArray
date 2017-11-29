a8f999d2ad9876d07bfd7ea5cb0859d70d08ca11
Andrius Shiaulis <shiaulis@gmail.com>
2017-11-29 21:28:57 +0200

    Initial Commit

---
+++ MutableArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MutableArray
+//
+//  Created by andrius on 29.11.17.
+//  Copyright © 2017 andrius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
